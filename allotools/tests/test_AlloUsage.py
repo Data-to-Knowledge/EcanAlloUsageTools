2dc7a72e175124b396f5fd0e97000b8c043d0159
MikeK <mgkittridge@gmail.com>
2019-06-29 18:19:57 +1200

    fixed plotting

--- allotools/tests/test_AlloUsage.py
+++ allotools/tests/test_AlloUsage.py
@@ -4,6 +4,7 @@ Created on Tue Oct  2 11:48:42 2018
 
 @author: michaelek
 """
+import os
 from allotools import AlloUsage
 import pandas as pd
 
@@ -20,6 +21,8 @@ cols = ['CatchmentName', 'WaterUse', 'Date']
 site_filter = {'SwazName': ['Selwyn-Waimakariri']}
 crc_filter = {'RecordNumber': ['CRC000354.3']}
 
+base_dir = os.path.realpath(os.path.dirname(__file__))
+
 ####################################
 ### Run tests
 
@@ -27,6 +30,8 @@ def test_AlloUsage1():
     a1 = AlloUsage(from_date, to_date, site_filter=site_filter)
     combo_ts1 = a1.get_ts(datasets, 'A-JUN', cols)
     combo_ts2 = a1.get_ts(datasets, 'M', cols)
+    a1.plot_group(freq, export_path=base_dir)
+    a1.plot_stacked(freq, export_path=base_dir)
 
     assert (len(combo_ts1) == 73) & (len(combo_ts2) == 869)
 
@@ -34,7 +39,7 @@ def test_AlloUsage2():
     a2 = AlloUsage(from_date, to_date, crc_filter=crc_filter)
     combo_ts3 = a2.get_ts(datasets, freq, cols)
 
-    assert (combo_ts3.SwAllo.sum() == 1480908) & (combo_ts3.SwUsage.sum() == 953874)
+    assert (combo_ts3.SwAllo.sum() == 1480908) & (combo_ts3.SwUsage.sum() == 1017923)
 
 
 
